0eeea570855752ae45b3337b1fa015ed35a69342
DESKTOP-F3TJJDB\bahat <bahauslusoy@gmail.com>
2022-07-01 17:32:51 +0300

    Add : CameraController Script

--- Assets/AAA/Scripts/CameraController.cs
+++ Assets/AAA/Scripts/CameraController.cs
@@ -1,19 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Ambrosia.EventBus;
 using UnityEngine;
+
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject target;
-    public Vector3 offset;
+    private Animator camAnimator;
+   //public GameObject target;
+    //public Vector3 offset;
     void Start()
     {
-
+        camAnimator = GetComponent<Animator>();
     }
 
+    private void OnEnable()
+    {
+        EventBus<EventCamControl>.AddListener(CameraChange);
+    }
+    private void OnDisable()
+    {
+        EventBus<EventCamControl>.RemoveListener(CameraChange);
+    }
+
+    private void CameraChange(object sender, EventCamControl @event)
+    {
+        camAnimator.Play("finishCam");
+        Debug.Log("cam");
+    }
 
     void LateUpdate()
     {
-        this.transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.deltaTime);
+       // this.transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.deltaTime);
     }
 }
