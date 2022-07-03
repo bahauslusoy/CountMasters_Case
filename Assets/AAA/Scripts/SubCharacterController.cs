9da1c1fde7efe987000774ed7bb1e4fcc4e0de22
DESKTOP-F3TJJDB\bahat <bahauslusoy@gmail.com>
2022-07-03 18:41:15 +0300

    Add : SubCharacter Script

--- Assets/AAA/Scripts/SubCharacterController.cs
+++ Assets/AAA/Scripts/SubCharacterController.cs
@@ -1,54 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
-
 
 public class SubCharacterController : MonoBehaviour
 {
-    GameObject Target;
-    NavMeshAgent navmesh;
+    private PlayerCount playerCount;
+    private CharController charController;
+
+    private Transform center;
+    private Rigidbody rb;
+
+    //private bool isFinished;
+    //public Animator CharAnim;
     void Start()
     {
-        navmesh = GetComponent<NavMeshAgent>();
-        Target = GameObject.FindWithTag("GameManager").GetComponent<GameManager>().ArrivalPoint;
+        rb = gameObject.GetComponent<Rigidbody>();
+        playerCount = GameObject.Find("Player").GetComponent<PlayerCount>();
+        charController = transform.parent.GetComponent<CharController>();
+
+        center = playerCount.transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        navmesh.SetDestination(Target.transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, center.position, Time.fixedDeltaTime * 1f);
+
+
     }
 
+   
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Obstacle"))
         {
+            GameObject.FindWithTag("GameManager").GetComponent<GameManager>().DestroyEffect(gameObject.transform);
+            playerCount.CharDead();
+            gameObject.SetActive(false);
+        }
 
+        else if (other.gameObject.CompareTag("Saw"))
+        {
+            playerCount.CharDead();
             GameObject.FindWithTag("GameManager").GetComponent<GameManager>().DestroyEffect(gameObject.transform);
             gameObject.SetActive(false);
         }
-
-        if (other.gameObject.CompareTag("Saw"))
+        else if (other.gameObject.CompareTag("Hammer"))
         {
-
-            GameObject.FindWithTag("GameManager").GetComponent<GameManager>().DestroyEffect(gameObject.transform);
+            playerCount.CharDead();
             gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Hammer"))
-        {
-
-            //GameObject.FindWithTag("GameManager").GetComponent<GameManager>().DestroyEffect(gameObject.transform);
-            GameObject.FindWithTag("GameManager").GetComponent<GameManager>().StainEffect(gameObject.transform);
-            gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Enemy"))
-        {
-
-            //GameObject.FindWithTag("GameManager").GetComponent<GameManager>().DestroyEffect(gameObject.transform);
-            GameObject.FindWithTag("GameManager").GetComponent<GameManager>().StainEffect(gameObject.transform,false);
-            gameObject.SetActive(false);
+            GameObject.FindWithTag("GameManager").GetComponent<GameManager>().StainEffect(gameObject.transform, true);
         }
     }
-
 }
