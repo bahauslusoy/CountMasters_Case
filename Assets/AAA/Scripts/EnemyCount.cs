a49148612c46f6882228730c9a8ad4e8abc4e398
DESKTOP-F3TJJDB\bahat <bahauslusoy@gmail.com>
2022-07-04 15:31:45 +0300

    Edit : EnemyCount Script

--- Assets/AAA/Scripts/EnemyCount.cs
+++ Assets/AAA/Scripts/EnemyCount.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EnemyCount : MonoBehaviour
 {
@@ -9,16 +10,20 @@ public class EnemyCount : MonoBehaviour
     public List<GameObject> enemies = new List<GameObject>();
 
     public bool isEnemyAlive = true;
+    public TextMeshProUGUI enemyCountText;
 
     void Start()
     {
         SpawnEnemy();
+        //CreateEnemiesAroundPoint();
     }
 
-    
+
     void Update()
     {
-        if(enemies.Count == 0)
+        enemyCountText.text = enemies.Count.ToString();
+
+        if (enemies.Count == 0)
         {
             isEnemyAlive = false;
         }
@@ -27,18 +32,49 @@ public class EnemyCount : MonoBehaviour
 
     public void SpawnEnemy()
     {
-        for(int i = 0; i <10 ; i++)
+
+        for (int i = 0; i < 10; i++)
         {
-            GameObject newEnemy = Instantiate(enemy,EnemyPosition(),Quaternion.identity, transform);
+            GameObject newEnemy = Instantiate(enemy, EnemyPosition(), Quaternion.identity, transform);
             enemies.Add(newEnemy);
         }
     }
     public Vector3 EnemyPosition()
     {
-        Vector3 pos = Random.insideUnitSphere * 0.5f ;
+        Vector3 pos = Random.insideUnitSphere * 0.5f;
         Vector3 newPos = transform.position + pos;
         newPos.y = 0.0f;
-        
+
         return newPos;
     }
+
+    /*public void CreateEnemiesAroundPoint()
+    {
+    float radius = 1f;
+    Vector3 pos = Random.insideUnitSphere * 0.5f;
+    Vector3 newPos = transform.position + pos;
+    float num = 10;
+    for (int i = 0; i < num; i++)
+    {
+
+        // Distance around the circle 
+        var radians = 2 * Mathf.PI / num * i;
+
+        // Get the vector direction 
+        var vertical = Mathf.Sin(radians);
+        var horizontal = Mathf.Cos(radians);
+
+        var spawnDir = new Vector3(horizontal, 0, vertical);
+
+        // Get the spawn position 
+        var spawnPos = newPos + spawnDir * radius; // Radius is just the distance away from the point
+
+        // Now spawn 
+        GameObject newEnemy = Instantiate(enemy, spawnPos, Quaternion.identity) as GameObject;
+
+
+
+
+    }
+}*/
 }
