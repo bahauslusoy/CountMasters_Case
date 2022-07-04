4738354fc17a3837c00628cc96da6982eb61c703
DESKTOP-F3TJJDB\bahat <bahauslusoy@gmail.com>
2022-07-04 15:27:59 +0300

    Edit : SubCharacterController Script

--- Assets/AAA/Scripts/SubCharacterController.cs
+++ Assets/AAA/Scripts/SubCharacterController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SubCharacterController : MonoBehaviour
 {
@@ -10,10 +11,13 @@ public class SubCharacterController : MonoBehaviour
     private Transform center;
     private Rigidbody rb;
 
+    public Image dangerImage;
+
     //private bool isFinished;
     public Animator CharAnim;
     void Start()
     {
+        dangerImage.enabled = false;
         CharAnim = GetComponent<Animator>();
         rb = gameObject.GetComponent<Rigidbody>();
         playerCount = GameObject.Find("Player").GetComponent<PlayerCount>();
@@ -34,10 +38,14 @@ public class SubCharacterController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Obstacle"))
         {
+
+
             GameObject.FindWithTag("GameManager").GetComponent<GameManager>().DestroyEffect(gameObject.transform);
             playerCount.CharDead();
             gameObject.SetActive(false);
             playerCount.characterList.Remove(gameObject);
+            StartCoroutine(DangerActive());
+
         }
 
         else if (other.gameObject.CompareTag("Saw"))
@@ -53,7 +61,13 @@ public class SubCharacterController : MonoBehaviour
             gameObject.SetActive(false);
             playerCount.characterList.Remove(gameObject);
             GameObject.FindWithTag("GameManager").GetComponent<GameManager>().StainEffect(gameObject.transform, true);
-            
+
         }
+    }
+    public IEnumerator DangerActive()
+    {
+        dangerImage.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        dangerImage.enabled = false ;
     }
 }
